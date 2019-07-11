35c92c62028534ea21083b6c1533d58ed0949a28
hongliao xiong <hongliaoxiong@hongliaos-MacBook-Air.local>
2019-07-11 10:48:56 -0700

    Updates

---
+++ precache-manifest.f8048355127eb28fda3ae3e03368381b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2506d729786a14967ab0cbe3acbfa36",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "b1e360f84a2417f4a751",
+    "url": "/clicky_game/static/css/main.374e85a8.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky_game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "b1e360f84a2417f4a751",
+    "url": "/clicky_game/static/js/main.c87defe7.chunk.js"
+  },
+  {
+    "revision": "ef6ad64b0df3315f3b6a",
+    "url": "/clicky_game/static/js/runtime~main.33627756.js"
+  },
+  {
+    "revision": "92686f4c191bb4371a99b394060525e4",
+    "url": "/clicky_game/static/media/Bugs_Bunny.92686f4c.png"
+  },
+  {
+    "revision": "c396feaf9a730becc618d8f6b5319b7c",
+    "url": "/clicky_game/static/media/Captain_Planet.c396feaf.png"
+  },
+  {
+    "revision": "4bfe4f7fadfed54ebb871a9a04cfc3fd",
+    "url": "/clicky_game/static/media/Courage.4bfe4f7f.png"
+  },
+  {
+    "revision": "9841fb6b961ba1e1d913a01cee69757b",
+    "url": "/clicky_game/static/media/Dexter.9841fb6b.png"
+  },
+  {
+    "revision": "f845edccc30bd14b995a9b41c3a5924e",
+    "url": "/clicky_game/static/media/Doug_Funnie.f845edcc.png"
+  },
+  {
+    "revision": "d320b495c90d72fa7928513ee46a596b",
+    "url": "/clicky_game/static/media/Johnny_Bravo.d320b495.png"
+  },
+  {
+    "revision": "7bc09bfc51113d3e7356ad53135b93f0",
+    "url": "/clicky_game/static/media/Mr._Krabs.7bc09bfc.png"
+  },
+  {
+    "revision": "e8dc09c9468e7e67e09543f9dacbc09a",
+    "url": "/clicky_game/static/media/Rockos.e8dc09c9.png"
+  },
+  {
+    "revision": "1ee2723fc6ca33e36dfbeae6fd58edaf",
+    "url": "/clicky_game/static/media/SpongeBob.1ee2723f.png"
+  },
+  {
+    "revision": "691be0cfa0475eafb29f0738490328cb",
+    "url": "/clicky_game/static/media/Squidward.691be0cf.png"
+  },
+  {
+    "revision": "141b3dcd2ac07cf9a6eca6826faa1285",
+    "url": "/clicky_game/static/media/Tommy_Pickles.141b3dcd.png"
+  },
+  {
+    "revision": "7d61ed40ed2f995f927058e02cf1e625",
+    "url": "/clicky_game/static/media/ickis.7d61ed40.png"
+  },
+  {
+    "revision": "b9da2244b48e97577590dd8137200f51",
+    "url": "/clicky_game/static/media/jumbotron-background.b9da2244.png"
+  }
+]);
